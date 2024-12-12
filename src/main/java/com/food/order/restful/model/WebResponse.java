a55455bd56974b28381557c2f3a2f9d28323bb08
bintangginanjar <bintang.ginanjar@gmail.com>
2024-12-12 13:46:58 +0700

    Adding ErrorController, WebResponse & ValidationService

---
+++ src/main/java/com/food/order/restful/model/WebResponse.java
@@ -0,0 +1,21 @@
+package com.food.order.restful.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+
+    private Boolean status;
+
+    private String messages;
+
+    private String errors;    
+
+    private T data;    
+}
