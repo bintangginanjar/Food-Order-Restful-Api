0b883fdfadaefd58a7dc7f9808fd3b1915d586a1
bintangginanjar <bintang.ginanjar@gmail.com>
2024-12-14 18:53:06 +0700

    Add create order and its unit testing

--- src/main/java/com/food/order/restful/repository/OrderRepository.java
+++ src/main/java/com/food/order/restful/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.food.order.restful.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.food.order.restful.entity.OrderEntity;
+import com.food.order.restful.entity.UserEntity;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
 
+    Optional<OrderEntity> findFirstByUserEntityAndId(UserEntity user, Integer orderId);
 }
--- src/test/java/com/food/order/restful/controller/OrderControllerTest.java
+++ src/test/java/com/food/order/restful/controller/OrderControllerTest.java
@@ -1,10 +1,101 @@
 package com.food.order.restful.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.food.order.restful.entity.CategoryEntity;
+import com.food.order.restful.entity.FoodEntity;
+import com.food.order.restful.entity.UserEntity;
+import com.food.order.restful.model.OrderResponse;
+import com.food.order.restful.model.WebResponse;
+import com.food.order.restful.repository.CategoryRepository;
+import com.food.order.restful.repository.FoodRepository;
+import com.food.order.restful.repository.OrderRepository;
+import com.food.order.restful.repository.ProfileRepository;
+import com.food.order.restful.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@EnableWebMvc
+@SpringBootTest
+@AutoConfigureMockMvc
 public class OrderControllerTest {
-    @Test
-    void testCreate() throws Exception {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private FoodRepository foodRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        orderRepository.deleteAll();
+        foodRepository.deleteAll();
+        categoryRepository.deleteAll();
+        profileRepository.deleteAll();
+        userRepository.deleteAll();
+
+        UserEntity user = new UserEntity();
+        user.setUsername("test");
+        user.setName("Test");
+        user.setPassword(BCrypt.hashpw("123456", BCrypt.gensalt()));
+        user.setToken("test");
+        user.setTokenExpiredAt(System.currentTimeMillis() + (1000 * 60 * 24 * 1));
+        userRepository.save(user);
+
+        CategoryEntity category = new CategoryEntity();
+        category.setName("Main Course");
+        categoryRepository.save(category);
+
+        FoodEntity food = new FoodEntity();
+        food.setName("Fried rice");
+        food.setPrice(40);
+        food.setPhotoUrl("https://img.freepik.com/free-photo/american-shrimp-fried-rice-served-with-chili-fish-sauce-thai-food_1150-26576.jpg");
+        food.setCategoryEntity(category);
+        foodRepository.save(food);
+    }
+
+    @Test
+    void testCreateOrderSuccess() throws Exception {                
+        mockMvc.perform(
+                post("/api/users/orders")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)                        
+                        .header("X-API-TOKEN", "test")                       
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+                WebResponse<OrderResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
+            });
+
+            assertEquals(true, response.getStatus());
+            assertNull(response.getErrors());
+        });
     }
 }
